d99f111d71146492d9bb7259377c00c8ad870239
Kyrylko <kkuzmenko1992@gmail.com>
2025-03-23 00:59:16 -0230

    test cors with localhost frontend

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import { PORT } from './config/env.js';
+import { PORT } from "./config/env.js";
 
-import oliveRouter from './routes/olive.routes.js';
-import authRouter from './routes/auth.routes.js';
-import userRouter from './routes/user.routes.js';
-import connectToDatabase from './database/mongodb.js';
-import errorMiddleware from './middlewares/error.middleware.js';
-import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import oliveRouter from "./routes/olive.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(arcjetMiddleware);
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/olives', oliveRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/olives", oliveRouter);
 
 app.use(errorMiddleware);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to api')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to api");
+});
 
 app.listen(PORT, async () => {
-    console.log(`api running on http://localhost:${PORT}`)
+  console.log(`api running on http://localhost:${PORT}`);
 
-    await connectToDatabase();
-})
+  await connectToDatabase();
+});
 
 export default app;
