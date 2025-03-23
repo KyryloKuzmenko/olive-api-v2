9dc84be461dc4874f450e5ad56e7b6e4e593f8de
Kyrylko <kkuzmenko1992@gmail.com>
2025-03-23 02:29:53 -0230

    test-2

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://localhost:5173",
     credentials: true,
   })
 );
