b0305f5ed84754d016d619333d10d70807e51574
Kyrylko <kkuzmenko1992@gmail.com>
2025-03-26 00:45:43 -0230

    added arcjet middleware to olive route

--- routes/olive.routes.js
+++ routes/olive.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import authorize from '../middlewares/auth.middleware.js';
 import { createOlive, getOlives } from "../controllers/olive.controller.js";
+import arcjetMiddleware from "../middlewares/arcjet.middleware.js";
 
 const oliveRouter = Router();
 
 oliveRouter.get('/', authorize, getOlives);
 
-oliveRouter.post('/', authorize, createOlive);
+oliveRouter.post('/', arcjetMiddleware, authorize, createOlive);
 
 export default oliveRouter;
