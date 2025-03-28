fd6b1a05e27094cce89ba487fa677e255649473d
Kyrylko <kkuzmenko1992@gmail.com>
2025-03-28 14:05:15 -0230

    added border for markers

--- controllers/olive.controller.js
+++ controllers/olive.controller.js
@@ -1,5 +1,9 @@
 import Olive from "../models/olive.model.js";
 
+import * as turf from "@turf/turf";
+import regionPolygon from "../data/cherkasyRegion.js";
+
+
 export const createOlive = async (req, res, next) => {
   try {
     const recent = await Olive.findOne({
@@ -11,6 +15,23 @@ export const createOlive = async (req, res, next) => {
       return res
         .status(429)
         .json({ message: "Please wait before adding another marker" });
+    }
+
+    // 2️⃣ Проверка, находится ли точка внутри допустимой области
+    const { coordinates } = req.body.location || {};
+    if (!coordinates || coordinates.length !== 2) {
+      return res.status(400).json({ message: "Invalid location coordinates" });
+    }
+
+    const point = turf.point(coordinates);
+    const isInside = turf.booleanPointInPolygon(point, regionPolygon);
+
+    if (!isInside) {
+      return res
+        .status(403)
+        .json({
+          message: "You can only place markers inside the allowed area",
+        });
     }
 
     const olive = await Olive.create({ ...req.body, user: req.user._id });
