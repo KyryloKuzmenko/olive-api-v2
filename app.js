53b1ab75a302bb836ef7413ee9d7f42c753774c2
Kyrylko <kkuzmenko1992@gmail.com>
2025-03-28 23:08:55 -0230

    testing

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
 
+// test
+import testRoute from "./routes/testRoute.js"
+// test
 const app = express();
 
 app.use(express.json());
@@ -27,6 +30,10 @@ app.use(arcjetMiddleware);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/olives", oliveRouter);
+
+// test
+app.use("/api/v1", testRoute)
+// test
 
 app.use(errorMiddleware);
 
